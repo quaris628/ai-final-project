102a569ac41eda202d11ff86965300ded956b5a1
Quaris <quaris314@gmail.com>
2022-04-15 06:26:24 -0500

    Added 2 comments to Tile.

--- ai-final-project-checkers/src/edu/ai/mainproj/checkers/Tile.java
+++ ai-final-project-checkers/src/edu/ai/mainproj/checkers/Tile.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 /**
  * A tile on the checkers board.
+ * May contain a piece.
  *
  * @author Nathan Swartz
  */
@@ -17,6 +18,13 @@ public class Tile {
     private final int column;
     private Piece piece; // null means blank
 
+    /**
+     * Creates a new tile, on a board, at a certain location
+     *     on the board's grid (specified by row and column)
+     * @param board
+     * @param row
+     * @param column
+     */
     public Tile(Board board, int row, int column) {
         this.board = board;
         this.row = row;
@@ -83,6 +91,11 @@ public class Tile {
     }
 
     // simple getters/setters
+
+    /**
+     * Tests if there is a piece on this tile.
+     * @return true if no piece is on the tile, otherwise false
+     */
     public boolean isBlank() { return piece == null; }
     public Piece getPiece() { return piece; }
     public void removePiece() { this.piece = null; }
