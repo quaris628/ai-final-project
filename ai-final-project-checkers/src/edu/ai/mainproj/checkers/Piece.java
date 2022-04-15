a8b8ebea0e8026aef0114968eed7835f4cdc5db2
Quaris <quaris314@gmail.com>
2022-04-15 15:16:42 -0500

    Added comments to Piece, wrote unit tests for Piece

--- ai-final-project-checkers/src/edu/ai/mainproj/checkers/Piece.java
+++ ai-final-project-checkers/src/edu/ai/mainproj/checkers/Piece.java
@@ -1,31 +1,73 @@
 package edu.ai.mainproj.checkers;
 
+/**
+ * A Piece, belonging to a certain player,
+ *     that can be placed and moved around Tiles
+ *
+ * Belongs to exactly one player, immutable
+ * May be moved to another tile, when moved
+ *     is no longer on previous tile
+ * May be removed from the board
+ *
+ * Starts unkinged
+ * May be kinged once, upon doing so is a king forever
+ *
+ * IS NOT RESTRICTED BY CHECKERS MOVEMENT RULES
+ * Maybe TODO
+ * idk what the best system is
+ *
+ * @author Nathan Swartz
+ */
 public class Piece {
 
     private Tile tile;
-    private PlayerType playerType;
+    private final PlayerType playerType;
     private boolean king;
 
+    /**
+     * Constructs a new Piece given the player who
+     *     owns it, and the tile it belongs to.
+     * @param playerType
+     * @param tile
+     */
     public Piece(PlayerType playerType, Tile tile) {
         this.tile = tile;
         this.playerType = playerType;
         this.king = false;
     }
 
+    /**
+     * Moves this piece to another tile
+     * DOES NOT check if the move is a
+     *     valid checkers move
+     * @param tile destination
+     */
     public void moveTo(Tile tile) {
-        this.tile.setPiece(null);
+        if (!tile.isBlank()) {
+            throw new IllegalArgumentException(
+                    "Destination tile already has a piece");
+        }
+        this.tile.removePiece();
         this.tile = tile;
         this.tile.setPiece(this);
         // Validation of moves, etc is in the Move class
     }
-	
+
+    /**
+     * Removes this piece from the tile it is on.
+     */
 	public void remove() {
-		this.tile.setPiece(null);
-		this.tile = null;
+		if (this.tile != null) {
+            this.tile.removePiece();
+            this.tile = null;
+        }
 	}
 	
     // simple getters/setters
     public boolean isKing() { return king; }
+    /**
+     * Kings this piece
+     */
 	public void king() { king = true; }
     public PlayerType getPlayer() { return playerType; }
 	public Tile getTile() { return tile; }
---
+++ ai-final-project-checkers/src/edu/ai/tests/checkers/PieceTests.java
@@ -0,0 +1,142 @@
+package edu.ai.tests.checkers;
+
+import edu.ai.mainproj.checkers.*;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit Tests for Piece class
+ *
+ * Tests methods:
+ *  - Constructor & getTile
+ *  - moveTo
+ *  - remove
+ *  - isKing
+ *  Tests indirectly / assumes functional:
+ *   - king
+ *  Does not test:
+ *   - getPlayer
+ *  
+ *
+ * @author Nathan Swartz
+ */
+public class PieceTests {
+
+    private final static Board blankBoard =
+            Board.CreateBlankBoard();
+
+    public PieceTests() {}
+
+    // --------------------------------
+    // CONSTRUCTOR / GET TILE
+    // --------------------------------
+
+    @Test
+    public void testGetTile() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Piece piece = new Piece(PlayerType.RED, tile01);
+
+        assertEquals(tile01, piece.getTile());
+        assertEquals(piece, tile01.getPiece());
+    }
+
+    // --------------------------------
+    // IS KING
+    // --------------------------------
+
+    @Test
+    public void testIsKing_01Red_False() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Piece piece = new Piece(PlayerType.RED, tile01);
+
+        assertFalse(piece.isKing());
+    }
+
+    @Test
+    public void testIsKing_01Black_True() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Piece piece = new Piece(PlayerType.BLACK, tile01);
+
+        assertTrue(piece.isKing());
+    }
+
+    @Test
+    public void testIsKing_70Red_True() {
+        Tile tile70 = blankBoard.getTile(7, 0);
+        Piece piece = new Piece(PlayerType.RED, tile70);
+
+        assertTrue(piece.isKing());
+    }
+
+    @Test
+    public void testIsKing_70Black_False() {
+        Tile tile70 = blankBoard.getTile(7, 0);
+        Piece piece = new Piece(PlayerType.BLACK, tile70);
+
+        assertFalse(piece.isKing());
+    }
+
+    @Test
+    public void testIsKing_70Red_Moved61_True() {
+        Tile tile70 = blankBoard.getTile(7, 0);
+        Tile tile61 = blankBoard.getTile(6, 1);
+        Piece piece = new Piece(PlayerType.RED, tile70);
+
+        piece.moveTo(tile61);
+
+        assertTrue(piece.isKing());
+    }
+
+
+    // --------------------------------
+    // REMOVE
+    // --------------------------------
+
+    @Test
+    public void testRemove() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Piece piece = new Piece(PlayerType.RED, tile01);
+
+        piece.remove();
+
+        assertNull(piece.getTile());
+        assertTrue(tile01.isBlank());
+    }
+
+    // --------------------------------
+    // MOVE TO
+    // --------------------------------
+
+    @Test
+    public void testMoveTo() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Tile tile10 = blankBoard.getTile(1, 0);
+        Piece piece = new Piece(PlayerType.RED, tile01);
+
+        piece.moveTo(tile10);
+
+        assertEquals(tile10, piece.getTile());
+        assertTrue(tile01.isBlank());
+        assertEquals(piece, tile10.getPiece());
+    }
+
+    @Test
+    public void testMoveTo_DestinationNotBlank_Exception() {
+        Tile tile01 = blankBoard.getTile(0, 1);
+        Tile tile10 = blankBoard.getTile(1, 0);
+        Piece piece01 = new Piece(PlayerType.RED, tile01);
+        Piece piece10 = new Piece(PlayerType.RED, tile10);
+
+        boolean exceptionTriggered = false;
+        try {
+            piece01.moveTo(tile10);
+        } catch (IllegalArgumentException e) {
+            exceptionTriggered = true;
+        }
+
+        assertTrue(exceptionTriggered);
+    }
+
+
+}
