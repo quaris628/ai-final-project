29eef03ec2a823041e877518366c537796817b50
Quaris <quaris314@gmail.com>
2022-04-26 13:19:24 -0500

    Wrote initial implementation for some Create overloads in CheckersMoveMultiJump

--- ai-final-project-checkers/src/edu/ai/mainproj/checkers/moves/CheckersMoveMultiJump.java
+++ ai-final-project-checkers/src/edu/ai/mainproj/checkers/moves/CheckersMoveMultiJump.java
@@ -47,17 +47,60 @@ public class CheckersMoveMultiJump extends CheckersMove {
 		return new CheckersMoveMultiJump(piece, tile, jumps);
 	}
 
-	// create a multi-jump that pre-pends the jumpBefore onto the jumps
+	/**
+	 * Create a multi-jump that pre-pends the jumpBefore onto the jumps
+	 * @param jumpBefore
+	 * @param jumps
+	 * @return new CheckersMoveMultiJump
+	 */
 	public static CheckersMoveMultiJump Create(
 			CheckersMoveJump jumpBefore, CheckersMoveMultiJump jumps) {
-		// TODO
-		return null;
+		// check pieces of jumpBefore and jumps are the same
+		if (jumpBefore.piece != jumps.piece) { return null; }
+		// check destination of jumpBefore is the start of the first jump of jumps
+		else if (jumpBefore.destination != jumps.jumps.get(0).startingTile) {
+			return null;
+		}
+		// if we're at this point everything should be okay to create
+		List<CheckersMoveJump> deepCopyJumps = new LinkedList<CheckersMoveJump>();
+		deepCopyJumps.add(jumpBefore);
+		deepCopyJumps.addAll(jumps.jumps);
+		return new CheckersMoveMultiJump(jumpBefore.piece, jumps.destination, deepCopyJumps);
 	}
 
-	// create a multi-jump that is only one jump
+	/**
+	 * Create a multi-jump that appends the jumpAfter onto the jumps
+	 * @param jumps
+	 * @param jumpAfter
+	 * @return new CheckersMoveMultiJump
+	 */
+	public static CheckersMoveMultiJump Create(
+			CheckersMoveMultiJump jumps, CheckersMoveJump jumpAfter) {
+		// check pieces of jumpAfter and jumps are the same
+		if (jumps.piece != jumpAfter.piece) { return null; }
+		// check destination of last jump of jumps is the same as the starting tile of jumpAfter
+		else if (jumps.jumps.get(jumps.jumps.size() - 1).destination != jumpAfter.startingTile) {
+			return null;
+		}
+		// if we're at this point everything should be okay to create
+		List<CheckersMoveJump> deepCopyJumps = new LinkedList<CheckersMoveJump>(jumps.jumps);
+		deepCopyJumps.add(jumpAfter);
+		return new CheckersMoveMultiJump(jumpAfter.piece, jumps.destination, deepCopyJumps);
+	}
+
+	/**
+	 * Create a multi-jump that is only one jump
+	 * Should be used only if it is only going to be used as
+	 *     one part in a whole multi-jump sequence with 2 or more jumps
+	 * @param jump
+	 * @return new CheckersMoveMultiJump
+	 */
 	public static CheckersMoveMultiJump Create(CheckersMoveJump jump) {
-		// TODO
-		return null;
+		// No need to check if this jump is impossible because
+		//     CheckersMoveJump should have already verified that.
+		List<CheckersMoveJump> jumps = new LinkedList<CheckersMoveJump>();
+		jumps.add(jump);
+		return new CheckersMoveMultiJump(jump.piece, jump.destination, jumps);
 	}
 	
 	protected CheckersMoveMultiJump(CheckersPiece piece,
@@ -68,6 +111,9 @@ public class CheckersMoveMultiJump extends CheckersMove {
 	
 	@Override
 	public boolean isValid() {
+		if (jumps.size() <= 1) {
+			return false;
+		}
 		for (CheckersMoveJump jump : jumps) {
 			if (!jump.isValid()) {
 				return false;
