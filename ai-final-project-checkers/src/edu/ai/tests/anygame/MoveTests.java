c84b38d0a78dbece194f75c41818929f7d269209
Quaris <quaris314@gmail.com>
2022-05-07 21:53:49 -0500

    Added MoveTests

--- ai-final-project-checkers/src/edu/ai/tests/anygame/MoveTests.java
+++ ai-final-project-checkers/src/edu/ai/tests/anygame/MoveTests.java
@@ -1,6 +1,7 @@
 package edu.ai.tests.anygame;
 
 import edu.ai.mainproj.anygame.GridBoard;
+import edu.ai.mainproj.anygame.Move;
 import edu.ai.mainproj.anygame.Piece;
 import edu.ai.mainproj.anygame.Tile;
 import edu.ai.mainproj.checkers.CheckersBoard;
@@ -27,8 +28,6 @@ import static org.junit.Assert.*;
  */
 public class MoveTests {
 
-    private static final GridBoard blankBoard = new GridBoard(8, 8);
-
     public MoveTests() {}
 
     // --------------------------------
@@ -36,23 +35,59 @@ public class MoveTests {
     // --------------------------------
 
     @Test
-    public void testConstructor_allNull_exception() {
-        // TODO
+    public void testConstructor_allNull_illegalArgumentException() {
+        try {
+            Move move = new Move(null, null);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
+
     }
 
     @Test
-    public void testConstructor_pieceNull_exception() {
-        // TODO
+    public void testConstructor_pieceNull_illegalArgumentException() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+
+        try {
+            Move move = new Move(null, dest);
+            fail();
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
     }
 
     @Test
-    public void testConstructor_dirNull_exception() {
-        // TODO
+    public void testConstructor_dirNull_illegalArgumentException() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+
+        try {
+            Move move = new Move(piece, null);
+            assertTrue(false);
+        } catch (IllegalArgumentException e) {
+            assertTrue(true);
+        }
     }
 
     @Test
     public void testConstructor_normal_succeeds() {
-        // TODO
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+
+        try {
+            Move move = new Move(piece, dest);
+            assertTrue(true);
+        } catch (IllegalArgumentException e) {
+            fail();
+        }
     }
 
     // --------------------------------
@@ -60,15 +95,53 @@ public class MoveTests {
     // --------------------------------
 
     @Test
-    public void testIsValid_destinationNotBlank_false() {
-        // TODO
+    public void testIsValid_destinationIsStart_false() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Piece piece = new Piece(start);
 
+        Move move = new Move(piece, start);
+
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+
+        assertFalse(move.isValid());
+    }
+
+    @Test
+    public void testIsValid_destinationNotBlank_false() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+        Move move = new Move(piece, dest);
+        new Piece(dest);
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertFalse(dest.isBlank());
+
+        assertFalse(move.isValid());
     }
 
     @Test
     public void testIsValid_destinationBlank_true() {
-        // TODO
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+        Move move = new Move(piece, dest);
 
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertTrue(dest.isBlank());
+
+        assertTrue(move.isValid());
     }
 
     // --------------------------------
@@ -77,12 +150,107 @@ public class MoveTests {
 
     @Test
     public void testExecute_destinationNotBlank_exception() {
-        // TODO
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+        Move move = new Move(piece, dest);
+        new Piece(dest);
+
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertFalse(dest.isBlank());
+        assertFalse(move.isValid());
+
+        try {
+            move.execute();
+            fail();
+        } catch (IllegalStateException e) {
+            assertTrue(true);
+        }
     }
 
     @Test
-    public void testExecute_destinationBlank() {
-        // TODO
+    public void testExecute_destinationBlank_succeeds() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Tile dest = board.getTile(0, 2);
+        Piece piece = new Piece(start);
+        Move move = new Move(piece, dest);
+
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertTrue(dest.isBlank());
+        assertTrue(move.isValid());
+
+        try {
+            move.execute();
+            assertTrue(true);
+        } catch (IllegalStateException e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testExecute_destinationBlank_movesPiece() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Piece piece = new Piece(start);
+        Tile dest = board.getTile(0, 2);
+        Move move = new Move(piece, dest);
+
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertTrue(dest.isBlank());
+        assertTrue(move.isValid());
+
+        move.execute();
+
+        assertTrue(start.isBlank());
+        assertFalse(dest.isBlank());
+        assertEquals(dest.getPiece(), piece);
+        assertEquals(piece.getTile(), dest);
+    }
+
+    // --------------------------------
+    // UNEXECUTE
+    // --------------------------------
+
+    @Test
+    public void testUnexecute_normal_movesPiece() {
+        GridBoard board = new GridBoard(4, 4);
+        Tile start = board.getTile(0, 0);
+        Piece piece = new Piece(start);
+        Tile dest = board.getTile(0, 2);
+        Move move = new Move(piece, dest);
+
+        // for my sanity
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertTrue(dest.isBlank());
+        assertTrue(move.isValid());
+
+        move.execute();
+
+        // for my sanity
+        assertTrue(start.isBlank());
+        assertFalse(dest.isBlank());
+        assertEquals(dest.getPiece(), piece);
+        assertEquals(piece.getTile(), dest);
+
+        move.unexecute();
+
+        assertFalse(start.isBlank());
+        assertEquals(start.getPiece(), piece);
+        assertEquals(piece.getTile(), start);
+        assertTrue(dest.isBlank());
     }
 
 }
